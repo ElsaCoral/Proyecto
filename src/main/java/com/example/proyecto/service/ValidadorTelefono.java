75142c486e50d60599fac3cd4fa213a3e2e2d053
ElsaCoral <coralwild1@gmail.com>
2023-12-07 14:44:27 -0600

    Proyecto agregado

---
+++ src/main/java/com/example/proyecto/service/ValidadorTelefono.java
@@ -0,0 +1,9 @@
+package com.example.proyecto.service;
+
+import org.springframework.stereotype.Service;
+@Service
+public class ValidadorTelefono {
+    public String limpiaNumero(String telefono) {
+        return telefono.replaceAll("[^0-9]", "");
+    }
+}
